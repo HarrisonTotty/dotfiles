2f9a3b5625c651f08c0d184cfbba65408c3b1227
Harrison Totty <harrisont@wolfram.com>
2018-12-31 17:54:21 -0600

    Alias emacs to no-window mode

--- templates/zsh/aliases.zsh
+++ templates/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # ~/.config/zsh/aliases.zsh
 # Contains custom aliases/functions for zsh
 
+alias emacs='emacs -nw'
 alias ls='ls --color=auto'
 alias neofetch='neofetch --w3m'
 alias netstat='ss -pantu | column -t'
