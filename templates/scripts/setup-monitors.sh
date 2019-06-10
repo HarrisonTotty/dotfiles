450e1894551db9ed94dfe4eabafdf44ec8d377c4
Harrison Totty <harrisont@wolfram.com>
2019-06-10 17:00:33 -0500

    Update monitors and mkdot keybinding

--- templates/scripts/setup-monitors.sh
+++ templates/scripts/setup-monitors.sh
@@ -7,11 +7,11 @@ if echo "$connected" | grep -q 'eDP1'; then
     if echo "$connected" | grep -q 'HDMI1' && echo "$connected" | grep -q 'DP2'; then
         xrandr \
             --output VIRTUAL1 --off \
-            --output eDP1 --primary --mode 1920x1080 --pos 1680x0 --rotate normal \
+            --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
             --output DP1 --off \
             --output HDMI2 --off \
-            --output HDMI1 --mode 1680x1050 --pos 3600x0 --rotate normal \
-            --output DP2 --mode 1680x1050 --pos 0x0 --rotate normal
+            --output HDMI1 --mode 1920x1080 --pos 3840x0 --rotate normal \
+            --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal
     else
         xrandr \
             --output VIRTUAL1 --off \
