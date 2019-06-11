28975e1d3b8bbe20e290f713f08a9e004528a627
Harrison Totty <harrisont@wolfram.com>
2019-06-11 09:31:12 -0500

    Update ZSH options

--- templates/zsh/options.zsh
+++ templates/zsh/options.zsh
@@ -4,34 +4,47 @@
 # History settings
 HISTFILE="$HOME/.zsh-history.log"
 if [ ! -f $HISTFILE ]; then
-    echo '' > "$HISTFILE"
+    touch "$HISTFILE"
 fi
-HISTSIZE=10000
-SAVEHIST=100000
-setopt INC_APPEND_HISTORY
-setopt EXTENDED_HISTORY
+HISTSIZE=50000
+SAVEHIST=50000
 
-# Automatically change directories if input corresponds to a valid directory
+# Record timestamp in histrory file.
+setopt extended_history
+
+# Delete duplicates first from the history file.
+setopt hist_ignore_dups
+
+# Ignore commands that start with a space.
+setopt hist_ignore_space
+
+# Add commands to the history file in order of execution.
+setopt inc_append_history
+
+# Share the history file across all terminals.
+setopt share_history
+
+# Automatically change directories if input corresponds to a valid directory.
 setopt autocd
 
-# Print errors if a filename doesn't match correctly
+# Print errors if a filename doesn't match correctly.
 setopt nomatch
 
-# Disable beeping
+# Disable beeping.
 unsetopt beep
 
-# Enable extended globbing
+# Enable extended globbing.
 setopt extendedglob
 
-# Enable autocompletion
+# Enable autocompletion.
 autoload -Uz compinit
 compinit
 zstyle ':completion:*' menu select
 
-# Enable comments
+# Enable comments.
 set -k
 
-# Setup zsh keybindings
+# Setup zsh keybindings.
 [[ -n "${terminfo[khome]}" ]] && bindkey "${terminfo[khome]}" beginning-of-line
 [[ -n "${terminfo[kend]}"  ]] && bindkey "${terminfo[kend]}"  end-of-line
 [[ -n "${terminfo[kich1]}" ]] && bindkey "${terminfo[kich1]}" overwrite-mode
