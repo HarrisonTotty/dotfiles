4542eb313d5f667903fec515efb21da51f01deda
Harrison Totty <harrisont@wolfram.com>
2019-10-27 16:06:18 -0500

    Add mkdot.sh script

---
+++ mkdot.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Wrapper script used to generate my dotfiles.
+
+set -e
+trap "exit 100" INT
+
+export TMPL_BASE_DIR='src'
+export TMPL_BLOCK_END_STR='%}'
+export TMPL_BLOCK_START_STR='{%'
+export TMPL_COMMENT_END_STR='#}'
+export TMPL_COMMENT_START_STR='{#'
+export TMPL_LOG_FILE='mkdot.log'
+export TMPL_LOG_LEVEL='info'
+export TMPL_LOG_MODE='overwrite'
+export TMPL_OUTPUT="$HOME/.config"
+export TMPL_VAR_END_STR='}}'
+export TMPL_VAR_START_STR='{{'
+
+if [ "$#" -lt 1 ]; then
+    echo "USAGE: ./mkdot.sh <template configuration file>|clean [...]"
+    exit 0
+fi
+
+if [ "$1" == "clean" ]; then
+    rm -f mkdot.log
+    exit 0
+elif [ ! -f "$1" ]; then
+    echo "Error: Specified template configuration file doesn't exist!"
+    exit 1
+fi
+
+if [ -f ./tmpl-binary ]; then
+    tmpl='./tmpl-binary'
+else
+    tmpl='tmpl'
+fi
+
+if [ "$#" -gt 1 ]; then
+    $tmpl "$1" "${@:2:$#}"
+else
+    $tmpl "$1"
+fi
