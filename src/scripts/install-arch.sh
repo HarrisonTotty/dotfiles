d717fa507580edaa0e4fa07ee7e94c2ea51cbe31
Harrison Totty <harrisont@wolfram.com>
2019-12-27 14:45:48 -0600

    Changes in plane

--- src/scripts/install-arch.sh
+++ src/scripts/install-arch.sh
@@ -5,13 +5,15 @@
 {%
   do require(
     'installer.aur_packages',
+    'installer.bootloader',
     'installer.drive',
     'installer.filesystems',
     'installer.hostname',
     'installer.packages',
     'installer.partitions',
     'installer.shell',
-    'installer.username'
+    'installer.username',
+    'installer.user_directories',
   )
 %}
 {% set n0ec = 'subprocess returned non-zero exit code.' %}
@@ -57,47 +59,54 @@ EC=2
 
 print_sec "Performing initial setup..."
 
+print_subsec "Verifying root privileges..."
+curr_user=$(whoami)
+if [ "$curr_user" != "root" ]; then
+    print_nosubsec_err "Error: This script must be run as root."
+    exit $EC
+fi
+
 {% if not installer.disable_uefi is defined or not installer.disable_uefi %}
 print_subsec "Verifying UEFI mode..."
 if [ ! -d "$efivars_dir" ]; then
-    print_nosubsec_err "Unable to verify UEFI mode - \"$efivars_dir\" does not exist."
+    print_nosubsec_err "Error: Unable to verify UEFI mode - \"$efivars_dir\" does not exist."
     exit $EC
 elif [ -z "$(ls -A $efivars_dir)" ]; then
-    print_nosubsec_err "Unable to verify UEFI mode - \"$efivars_dir\" is empty."
+    print_nosubsec_err "Error: Unable to verify UEFI mode - \"$efivars_dir\" is empty."
     exit $EC
 fi
 {% endif %}
 
 print_subsec "Verifying internet connection..."
 if ! host "archlinux.org" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to verify internet connection - host lookup for \"archlinux.org\" failed."
+    print_nosubsec_err "Error: Unable to verify internet connection - host lookup for \"archlinux.org\" failed."
     exit $EC
 elif ! wget -q --spider "archlinux.org"; then
-    print_nosubsec_err "Unable to verify internet connection - connection to \"archlinux.org\" failed."
+    print_nosubsec_err "Error: Unable to verify internet connection - connection to \"archlinux.org\" failed."
     exit $EC
 fi
 
 print_subsec "Updating system clock..."
 if ! timedatectl set-ntp true >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to update system clock - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to update system clock - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Backing-up previous pacman mirror list..."
 if ! cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to back-up previous pacman mirror list - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to back-up previous pacman mirror list - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Installing rankmirrors package..."
 if ! pacman -Sy pacman-contrib --noconfirm >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to install rankmirrors package - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to install rankmirrors package - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Fetching and ranking fastest mirrors..."
 if ! curl -s "$mirrorlist_url" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - >/etc/pacman.d/mirrorlist 2>/dev/null; then
-    print_nosubsec_err "Unable to generate pacman mirrorlist - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to generate pacman mirrorlist - {{ n0ec }}"
     exit $EC
 fi
 
@@ -122,17 +131,17 @@ fi
 
 print_subsec "Probing existing disk partitions..."
 if ! partprobe "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to probe existing disk partitions - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to probe existing disk partitions - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Cleaning partition table..."
 if ! sgdisk --zap-all "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to zap partition table - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to zap partition table - {{ n0ec }}"
     exit $EC
 fi
 if ! sgdisk --clear "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to clear partition table - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to clear partition table - {{ n0ec }}"
     exit $EC
 fi
 
@@ -147,11 +156,11 @@ print_subsec "Creating \"{{ p.name }}\" partition..."
 {% set psize = '0' %}
 {% endif %}
 if ! sgdisk --new={{ loop.index }}:0:{{ psize }} "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create new partition - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create new partition - {{ n0ec }}"
     exit $EC
 fi
 if ! sgdisk --typecode={{ loop.index }}:{{ p.typecode|default('8300', true) }} "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to set partition type - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set partition type - {{ n0ec }}"
     exit $EC
 fi
 {% if p.encrypted is defined and p.encrypted %}
@@ -160,14 +169,14 @@ fi
 {% set pname = p.name %}
 {% endif %}
 if ! sgdisk --change-name={{ loop.index }}:{{ pname }} "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to set partition name - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set partition name - {{ n0ec }}"
     exit $EC
 fi
 {% endfor %}
 
 print_subsec "Informing kernel of partition changes..."
 if ! partprobe "{{ installer.drive }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to inform kernel of partition changes - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to inform kernel of partition changes - {{ n0ec }}"
     exit $EC
 fi
 
@@ -195,18 +204,18 @@ if [ -e "/dev/mapper/{{ p.name }}" ]; then
 fi
 {% if p.typecode is defined and p.typecode == '8200' %}
 if ! $cryptswapcmd "/dev/disk/by-partlabel/{{ p.name + '-encrypted' }}" "{{ p.name }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to encrypt swap partition - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to encrypt swap partition - {{ n0ec }}"
     exit $EC
 fi
 {% else %}
 cipher="{{ p.cipher|default('aes-xts-plain64', true) }}"
 keysize="{{ p.keysize|default('256', true) }}"
 if ! $cryptcmd --cipher $cipher --key-size $keysize "/dev/disk/by-partlabel/{{ p.name + '-encrypted' }}"; then
-    print_nosubsec_err "Unable to encrypt partition - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to encrypt partition - {{ n0ec }}"
     exit $EC
 fi
 if ! cryptsetup open "/dev/disk/by-partlabel/{{ p.name + '-encrypted' }}" "{{ p.name }}"; then
-    print_nosubsec_err "Unable to unseal encrypted partition - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to unseal encrypted partition - {{ n0ec }}"
     exit $EC
 fi
 {% endif %}
@@ -247,17 +256,17 @@ print_subsec "[{{ fs.kind }}] Creating \"{{ fs.name }}\" filesystem..."
 {% endif %}
 {% if fs.kind == 'btrfs' %}
 if ! mkfs.btrfs --force --label "{{ fs.name }}" "{{ partition_path }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% elif fs.kind == 'fat32' %}
 if ! mkfs.vfat -F 32 -n "{{ fs.name }}" "{{ partition_path }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% elif fs.kind == 'swap' %}
 if ! mkswap --label "{{ fs.name }}" "{{ partition_path }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% else %}
@@ -272,13 +281,13 @@ if [ -d "{{ fs.mountpoint }}" ]; then
     rm -rf "{{ fs.mountpoint }}" >> install-arch.log 2>&1
 fi
 if ! mkdir -p "{{ fs.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create filesystem mount directory - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create filesystem mount directory - {{ n0ec }}"
     exit $EC
 fi
 {% endif %}
 {% if fs.kind == 'btrfs' %}
 if ! mount -t btrfs "LABEL={{ fs.name }}" "{{ fs.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to mount filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to mount filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% if not fs.subvolumes is defined %}
@@ -290,13 +299,13 @@ print_subsec "[{{ fs.kind }}] Creating \"{{ fs.name }}\" subvolumes..."
 {% do raise('subvolume does not specify a name') %}
 {% endif %}
 if ! btrfs subvolume create "{{ path_join(fs.mountpoint, sv.name) }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create subvolume \"{{ sv.name }}\" - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create subvolume \"{{ sv.name }}\" - {{ n0ec }}"
     exit $EC
 fi
 {% endfor %}
 print_subsec "[{{ fs.kind }}] Mounting \"{{ fs.name }}\" filesystem subvolumes..."
 if ! umount -R "{{ fs.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to unmount subvolume owner - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to unmount subvolume owner - {{ n0ec }}"
     exit $EC
 fi
 {% for sv in fs.subvolumes %}
@@ -305,24 +314,24 @@ if [ -d "{{ sv.mountpoint }}" ]; then
     rm -rf "{{ sv.mountpoint }}" >> install-arch.log 2>&1
 fi
 if ! mkdir -p "{{ sv.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to create filesystem subvolume mount directory - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create filesystem subvolume mount directory - {{ n0ec }}"
     exit $EC
 fi
 mountcmd="mount -t btrfs -o subvol={{ sv.name }},{{ sv.mount_options|default('defaults', true) }}"
 if ! $mountcmd "LABEL={{ fs.name }}" "{{ sv.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to mount subvolume \"{{ sv.name }}\" - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to mount subvolume \"{{ sv.name }}\" - {{ n0ec }}"
     exit $EC
 fi
 {% endif %}
 {% endfor %}
 {% elif fs.kind == 'fat32' %}
 if ! mount "LABEL={{ fs.name }}" "{{ fs.mountpoint }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to mount filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to mount filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% elif fs.kind == 'swap' %}
 if ! swapon -L "{{ fs.name }}" >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to mount filesystem - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to mount filesystem - {{ n0ec }}"
     exit $EC
 fi
 {% endif %}
@@ -340,7 +349,7 @@ EC=6
 print_sec "Installing system packages..."
 
 if ! pacstrap /mnt {{ installer.packages|join(' ') }} >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to install system packages - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to install system packages - {{ n0ec }}"
     exit $EC
 fi
 
@@ -358,65 +367,187 @@ chroot='arch-chroot /mnt'
 
 print_subsec "Generating filesystem table..."
 if ! genfstab -U /mnt >> /mnt/etc/fstab 2>>install-arch.log; then
-    print_nosubsec_err "Unable to generate filesystem table - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to generate filesystem table - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting time zone..."
 if ! $chroot ln -sf "/usr/share/zoneinfo/{{ installer.timezone|default('America/Chicago', true) }}" /etc/localtime >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to set time zone - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set time zone - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting hardware clock..."
 if ! $chroot hwclock --verbose --systohc >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to set hardware clock - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set hardware clock - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Generating localization..."
 if ! echo 'en_US.UTF-8 UTF-8' > /mnt/etc/locale.gen && $chroot locale-gen >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to generate localization - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to generate localization - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting system language..."
 if ! echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf; then
-    print_nosubsec_err "Unable to set system language - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set system language - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting system hostname..."
 if ! echo '{{ installer.hostname }}' > /mnt/etc/hostname && $chroot hostname '{{ installer.hostname }}' >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unable to set system hostname - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set system hostname - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Adding system hostname to hosts file..."
 hosts_entry='127.0.0.1 {{ installer.hostname }}.localdomain {{ installer.hostname }}'
 if ! echo "$hosts_entry" >> /mnt/etc/hosts 2>>install-arch.log; then
-    print_nosubsec_err "Unable to add system hostname to hosts file - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to add system hostname to hosts file - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting root user account password..."
 if ! $chroot passwd 2>>install-arch.log; then
-    print_nosubsec_err "Unable to set root user account password - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set root user account password - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Creating \"{{ installer.username }}\" user account..."
 useraddcmd='useradd -m -g wheel -s "{{ installer.shell }}" "{{ installer.username }}"'
 if ! $chroot $useraddcmd >> install-arch.log 2>&1; then
-    print_nosubsec_err "Unsable to create primary user account - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to create primary user account - {{ n0ec }}"
     exit $EC
 fi
 
 print_subsec "Setting \"{{ installer.username }}\" user account password..."
 if ! $chroot passwd "{{ installer.username }}" 2>>install-arch.log; then
-    print_nosubsec_err "Unable to set primary user account password - {{ n0ec }}"
+    print_nosubsec_err "Error: Unable to set primary user account password - {{ n0ec }}"
     exit $EC
 fi
 
+print_subsec "Creating user home directories..."
+for d in {{ installer.user_directories|join(' ') }}; do
+    if ! mkdir -p "/mnt/home/{{ installer.username }}/$d" >> install-arch.log 2>&1; then
+        print_nosubsec_err "Error: Unable to create directory \"/mnt/home/{{ installer.username }}/$d\" - {{ n0ec }}"
+        exit $EC
+    fi
+done
+
+print_subsec "Setting \"{{ installer.username }}\" user account home permissions..."
+if ! $chroot chown -R {{ installer.username }}:wheel "/home/{{ installer.username }}" >> install-arch.log 2>&1; then
+    print_nosubsec_err "Error: Unable to set primary user account home permissions - {{ n0ec }}"
+    exit $EC
+fi
+
+# ----------------------------
+
+
+
+# ----- Setup Bootloader -----
+
+EC=8
+
+print_sec "Setting-up bootloader..."
+
+{% if not installer.bootloader.kind is defined %}
+{% do raise('bootloader kind not specified') %}
+{% endif %}
+{% if not installer.bootloader.root_partition is defined %}
+{% do raise('bootloader root partition not specified') %}
+{% endif %}
+
+{% if installer.bootloader.kind == 'systemd-boot' %}
+{% if not installer.bootloader.kernel_parameters is defined %}
+{% do raise('bootloader kernel parameters not specified') %}
+{% endif %}
+print_subsec "Installing bootloader (systemd-boot)..."
+if ! $chroot bootctl --path=/boot install >> install-arch.log 2>&1; then
+    print_nosubsec_err "Error: Unable to install bootloader - {{ n0ec }}"
+    exit $EC
+fi
+print_subsec "Configuring bootloader updates..."
+if [ ! -d /mnt/etc/pacman.d/hooks ]; then
+    if ! mkdir -p /mnt/etc/pacman.d/hooks >> install-arch.log 2>&1; then
+        print_nosubsec_err "Error: Unable to configure bootloader updates - unable to create package manager \"hooks\" directory."
+        exit $EC
+    fi
+fi
+cat > /mnt/etc/pacman.d/hooks/systemd-boot.hook 2>>install-arch.log <<EOF
+[Trigger]
+Type = Package
+Operation = Upgrade
+Target = systemd
+
+[Action]
+Description = Updating bootloader
+When = PostTransaction
+Exec = /usr/bin/bootctl update
+EOF
+if [ "$?" -ne 0 ]; then
+    print_nosubsec_err "Error: Unable to configure bootloader updates - unable to write package manager hook."
+    exit $EC
+fi
+print_subsec "Writing bootloader configuration..."
+if [ ! -d /mnt/boot/loader ]; then
+    if ! mkdir -p /mnt/boot/loader >> install-arch.log 2>&1; then
+        print_nosubsec_err "Error: Unable to write bootloader configuration - unable to create \"/boot/loader\" directory."
+        exit $EC
+    fi
+fi
+cat > /mnt/boot/loader/loader.conf 2>>install-arch.log <<EOF
+default arch
+timeout 1
+editor 0
+EOF
+if [ "$?" -ne 0 ]; then
+    print_nosubsec_err "Error: Unable to write bootloader configuration."
+    exit $EC
+fi
+print_subsec "Writing bootloader entry..."
+if [ ! -d /mnt/boot/loader/entries ]; then
+    if ! mkdir -p /mnt/boot/loader/entries >> install-arch.log 2>&1; then
+        print_nosubsec_err "Error: Unable to write bootloader entry - unable to create bootloader entries directory."
+        exit $EC
+    fi
+fi
+cat > /mnt/boot/loader/entries/arch.conf 2>>install-arch.log <<EOF
+title   {{ installer.bootloader.title|default('Arch Linux', true) }}
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+EOF
+if [ "$?" -ne 0 ]; then
+    print_nosubsec_err "Error: Unable to write bootloader entry - unable to create \"/boot/loader/entries/arch.conf\"."
+    exit $EC
+fi
+print_subsec "Configuring boot options..."
+{% if installer.system_encrypted is defined and installer.system_encrypted %}
+boot_root="root=PARTLABEL={{ installer.bootloader.root_partition }}"
+{% else %}
+boot_root="root=PARTLABEL={{ installer.bootloader.root_partition }}"
+{% endif %}
+boot_options="options $boot_root rw {{ installer.bootloader.kernel_parameters }}"
+if ! echo "$boot_options" >> /mnt/boot/loader/entries/arch.conf 2>>install-arch.log; then
+    print_nosubsec_err "Error: Unable to configure boot options - unable to append \"/boot/loader/entries/arch.conf\"."
+    exit $EC
+fi
+{% elif installer.bootloader.kind == 'grub' %}
+{% else %}
+{% do raise('unknown bootloader kind specified') %}
+{% endif %}
+
+# ----------------------------
+
+
+
+# -------- Completion --------
+
+print_sec "Installation complete."
+
+echo
+echo "Ensure that the installation process was successful and then run \"umount -R /mnt\" before rebooting."
+exit 0
 
 # ----------------------------
