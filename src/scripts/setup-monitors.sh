277898d41b6125f11b5b2627ff36e0199b2d3dea
Harrison Totty <harrisont@wolfram.com>
2020-01-06 14:49:35 -0600

    Improve monitor setup script

--- src/scripts/setup-monitors.sh
+++ src/scripts/setup-monitors.sh
@@ -14,19 +14,19 @@ if echo "$connected" | grep -q '{{ monitors.primary }}'; then
     if echo "$connected" | grep -q '{{ monitors.left }}' && echo "$connected" | grep -q '{{ monitors.right }}'; then
         xrandr \
             --output VIRTUAL1 --off \
-            --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+            --output "{{ monitors.primary }}" --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
             --output DP-1 --off \
             --output HDMI-2 --off \
-            --output HDMI-1 --mode 1920x1080 --pos 3840x0 --rotate normal \
-            --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal
+            --output "{{ monitors.right }}" --mode 1920x1080 --pos 3840x0 --rotate normal \
+            --output "{{ monitors.left }}" --mode 1920x1080 --pos 0x0 --rotate normal
     else
         xrandr \
             --output VIRTUAL1 --off \
-            --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+            --output "{{ monitors.primary }}" --primary --mode 1920x1080 --pos 0x0 --rotate normal \
             --output DP-1 --off \
             --output HDMI-2 --off \
-            --output HDMI-1 --off \
-            --output DP-2 --off
+            --output "{{ monitors.right }}" --off \
+            --output "{{ monitors.left }}" --off
     fi
 else
     echo "Primary display is not connected..."
