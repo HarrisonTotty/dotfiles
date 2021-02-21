a897f6791eee7cc6469b4661fac7d6e7b631e1f8
Harrison Totty <harrison.totty@centro.net>
2021-02-20 21:08:21 -0600

    Bunch of changes

--- src/scripts/ipython.sh
+++ src/scripts/ipython.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Simple wrapper script around running IPython
-{% do require('font.name') %}
+{% do require('font.name', 'terminal_emulator') %}
 
+{% if terminal_emulator == 'urxvt' %}
 urxvtc +sb \
        -title 'Python Interpreter' \
        -fn 'xft:{{ font.name }}:size=14' \
        -e "$SHELL" \
        -c "ipython --config $HOME/.config/ipy/config.py --no-banner"
+{% endif %}
--- src/scripts/ssh.sh
+++ src/scripts/ssh.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+{% do require('terminal_emulator') %}
+
+{% if terminal_emulator == 'urxvt' %}
 rofi -show ssh -ssh-command "urxvtc +sb -letsp 1 -title 'SSH : {host}' -e $HOME/.config/scripts/ssh-wrapper.sh {host}"
+{% endif %}
