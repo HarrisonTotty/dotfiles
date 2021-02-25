2844eae311e2c6de30b0c3896442d476d8aac7e7
Harrison Totty <harrison.totty@centro.net>
2021-02-25 15:13:19 -0600

    Add alacritty IPython support

--- src/scripts/ipython.sh
+++ src/scripts/ipython.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Simple wrapper script around running IPython
 {% do require('font.name', 'terminal_emulator') %}
+{% set wtitle = 'Python Interpreter' %}
 
 {% if terminal_emulator == 'urxvt' %}
 urxvtc +sb \
-       -title 'Python Interpreter' \
+       -title '{{ wtitle }}' \
        -fn 'xft:{{ font.name }}:size=14' \
        -e "$SHELL" \
        -c "ipython --config $HOME/.config/ipy/config.py --no-banner"
+{% elif terminal_emulator == 'alacritty' %}
+alacritty \
+    --title '{{ wtitle }}' \
+    --command /usr/bin/ipython --config "$HOME/.config/ipy/config.py" --no-banner
 {% endif %}
