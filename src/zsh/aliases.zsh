70b227a51e6c1e50b872c26368141c6ebf6d7c3d
Harrison Totty <harrison.totty@centro.net>
2021-02-25 15:13:44 -0600

    Add zsh support for alacritty

--- src/zsh/aliases.zsh
+++ src/zsh/aliases.zsh
@@ -1,7 +1,11 @@
 # ZSH Aliases
 # -----------
+{% do require('terminal_emulator') %}
 
 alias emacs='emacs -nw'
 alias ls='ls --color=auto'
+{% if terminal_emulator == 'urxvt' %}
 alias neofetch='neofetch --w3m'
+{% elif terminal_emulator == 'alacritty' %}
+{% endif %}
 alias netstat='ss -pantu | column -t'
