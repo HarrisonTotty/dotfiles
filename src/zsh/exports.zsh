641d058d7fc1289f7954911bafc29ea3c7e5fd86
Harrison Totty <harrison.totty@centro.net>
2021-02-28 15:50:58 -0600

    Explicitly set PATH better

--- src/zsh/exports.zsh
+++ src/zsh/exports.zsh
@@ -5,4 +5,4 @@ export EDITOR='emacs'
 export GOPATH="$HOME/.go"
 export LANG='en_US.UTF-8'
 export LC_CTYPE='en_US.UTF-8'
-export PATH="${HOME}/bin:${PATH}"
+export PATH="${HOME}/bin:${HOME}/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
