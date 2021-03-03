be04e72d0e22699218fbab80c5376fbd9bdc6224
Harrison Totty <harrison.totty@centro.net>
2021-03-03 12:57:41 -0600

    Make PATH more modular and add .local/bin

--- src/zsh/exports.zsh
+++ src/zsh/exports.zsh
@@ -1,8 +1,9 @@
 # ZSH Export Statements
 # ---------------------
+{% do require('bin_paths') %}
 
 export EDITOR='emacs'
 export GOPATH="$HOME/.go"
 export LANG='en_US.UTF-8'
 export LC_CTYPE='en_US.UTF-8'
-export PATH="${HOME}/bin:${HOME}/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
+export PATH="{{ bin_paths|join(':') }}"
